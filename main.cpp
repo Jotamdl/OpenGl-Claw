1395694491d06cd617f2d518233ab9427f287f07
João Hugo Martins da Luz <joaohugomartinsdaluz@gmail.com>
2024-12-13 00:32:21 -0300

    adding textures, a house and correcting bugs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <GL/glut.h>
 #include <cmath>
 #include <initializer_list> // Para listas de inicialização
+
 #include <math.h>
+#define PI 3.14159265358979323846
+#define NUM_SEGMENTOS 100
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <texture/stb_image.h>
 
 //---------------------------------- VARIÁVEIS E FUNÇÕES DA GARRA -----------------------------------
 
@@ -16,6 +22,12 @@ float anguloArticulacao3Y = 0.0;
 float anguloPegada = 0.0;
 float pegar = false;
 
+// angulos da camera
+float cameraAngleHorizontal = 0.0;
+float cameraAngleVertical = 45.0;
+float cameraDistance = 5.0;
+float cameraHeight = 0.0 ;
+
 // Limites para o ângulo de pegada
 const float ANGOLO_PEGADA_MAX = 75.0f;  // máximo de fechamento
 const float ANGOLO_PEGADA_MIN = 0.0f;   // mínimo ao abrir
@@ -27,6 +39,24 @@ float objetoX = 1.0f;
 float objetoY = 1.2f;   
 float objetoZ = 0.0f;
 bool objetoPegado = false;
+
+// variavel para carregar texturas
+GLuint idsTextura[10];
+bool loadHouseTex = false;
+
+void desenhaCirculo(float raio)
+{
+ //GL_LINE_LOOP
+ glBegin(GL_POLYGON);
+   for (int i = 0; i <= NUM_SEGMENTOS; i++)
+   {
+    float angulo = 2.0f * PI * i / NUM_SEGMENTOS;
+    float dx = raio * cos(angulo);
+    float dy = raio * sin(angulo);
+    glVertex2f(dx, dy);
+   }
+ glEnd();
+}
 
 // função auxiliar para desenhar pirâmide usada nos "dedos"
 void solidPyramid(float height, float baseSize) {
@@ -68,14 +98,85 @@ void solidPyramid(float height, float baseSize) {
   glPopMatrix();
 }
 
+// funcao auxiliar para desenhar cubos que podem receber texturas
+void solidCube(float size) {
+  size = size/2;
+  glPushMatrix();
+    // face frontal
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, size);
+
+    // face traseira
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, -size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, -size);
+
+    // face superior
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, size);
+
+    // face inferior
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, -size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, -size, size);
+
+    // face direita
+      glTexCoord2f(0.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(size, -size, size);
+
+    // face esquerda
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(-size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(-size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, -size, size);
+    glEnd();    
+    
+  glPopMatrix();
+}
+
+void carregarTextura(const char* nomeArquivo, int indice) {
+  int largura, altura, canais;
+
+  unsigned char *dados = stbi_load(nomeArquivo, &largura, &altura, &canais, 0);
+
+  if (!dados)
+    exit(1);
+
+  // gerar textura
+  glGenTextures(1, &idsTextura[indice]);
+  glBindTexture(GL_TEXTURE_2D, idsTextura[indice]);
+
+  // configurar parametros da textura
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, (canais == 4) ? GL_RGBA : GL_RGB, 
+                largura, altura, 0, (canais == 4) ? GL_RGBA : GL_RGB,
+                GL_UNSIGNED_BYTE, dados);
+
+  // liberar a memoria da imagem
+  stbi_image_free(dados);              
+}
+
 void desenhaGarra(GLfloat raio) {
   GLUquadricObj *quadratic = gluNewQuadric();
   gluQuadricNormals(quadratic, GLU_SMOOTH);
 
   // Materiais
-  GLfloat difusoBraco[] = {0.5f, 0.5f, 0.5f, 1.0f};
-  GLfloat ambienteBraco[] = {0.2f, 0.2f, 0.2f, 1.0f};
-  GLfloat especularBraco[] = {0.2f, 0.2f, 0.2f, 1.0f};
+  GLfloat difusoBraco[] = {0.7f, 0.7f, 0.7f, 1.0f};
+  GLfloat ambienteBraco[] = {0.4f, 0.4f, 0.4f, 1.0f};
+  GLfloat especularBraco[] = {0.4f, 0.4f, 0.4f, 1.0f};
 
   GLfloat difusoJunta[] = {0.4f, 0.4f, 0.4f, 1.0f};
   GLfloat ambienteJunta[] = {0.15f, 0.15f, 0.15f, 1.0f};
@@ -97,6 +198,12 @@ void desenhaGarra(GLfloat raio) {
       glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
       glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
       gluCylinder(quadratic, 0.5, 0.5, 0.13, 20, 20);
+    glPopMatrix();
+    
+    // topo cilindro base
+    glPushMatrix();
+      glRotatef(90.0, 1.0, 0.0, 0.0);
+      desenhaCirculo(0.5);
     glPopMatrix();
     
     // rotação articulação maior
@@ -231,13 +338,139 @@ void desenhaGarra(GLfloat raio) {
   gluDeleteQuadric(quadratic);
 }
 
-float cameraAngleY = 0.0f; 
-float cameraAngleX = 0.0f; 
-float cameraDistance = 10.0f; 
+void desenhaMesa() {
+  // Configurando texturas
+  glEnable(GL_TEXTURE_2D);
+  carregarTextura("textures/mesa_textura.jpg", 0);
+  // Ajustar modo para a textura não escurecer
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE); 
+  
+  GLfloat difusoMarrom[]    = {0.4f, 0.2f, 0.0f, 1.0f};
+  GLfloat ambienteMarrom[]  = {0.2f, 0.1f, 0.0f, 1.0f};
+  GLfloat especularMarrom[] = {0.0f, 0.0f, 0.0f, 1.0f}; 
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, difusoMarrom);
+  glMaterialfv(GL_FRONT, GL_AMBIENT, ambienteMarrom);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, especularMarrom);
+  glMaterialf(GL_FRONT, GL_SHININESS, 0.0f);
 
-float mesaRotX = 0.0f;
-float mesaRotY = 0.0f;
-float mesaRotZ = 0.0f;
+  glPushMatrix();
+    // adicionando textura
+    glBindTexture(GL_TEXTURE_2D, idsTextura[0]);
+
+    // Tampo da mesa
+    glPushMatrix();
+    glTranslatef(0.0f, 1.0f, 0.0f);
+    glScalef(4.0f, 0.2f, 2.0f);
+    solidCube(1.0f);
+    glPopMatrix();
+
+    // Pés da mesa
+    float tamPé = 0.2f; 
+    float alturaPé = 1.0f;
+
+    for (float x : std::initializer_list<float>{-1.7f, 1.7f}) {
+      for (float z : std::initializer_list<float>{-0.8f, 0.8f}) {
+        glPushMatrix();
+        glTranslatef(x, alturaPé / 2, z);
+        glScalef(tamPé, alturaPé, tamPé);
+        solidCube(1.0f);
+        glPopMatrix();
+      }
+    }
+    
+  glPopMatrix();
+  glDisable(GL_TEXTURE_2D);
+
+  // Desenhar o cubo sobre a mesa se não estiver pegado
+  if (!objetoPegado) {
+    GLfloat difObj[] = {0.8f, 0.1f, 0.1f, 1.0f};
+    GLfloat ambObj[] = {0.2f, 0.05f, 0.05f, 1.0f};
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, difObj);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambObj);
+    glMaterialf(GL_FRONT, GL_SHININESS, 10.0f);
+
+    glPushMatrix();
+      glTranslatef(objetoX, objetoY, objetoZ); 
+      glutSolidCube(0.1f);
+    glPopMatrix();
+  }
+
+  // Desenhar a garra acima da mesa
+  glPushMatrix();
+    glTranslatef(0.0f, 1.243f, 0.0f); 
+    desenhaGarra(1.0f); 
+  glPopMatrix();
+}
+
+void desenhaQuarto() {
+  // Configurando texturas
+  glEnable(GL_TEXTURE_2D);
+  carregarTextura("textures/parede.jpg", 1);
+  carregarTextura("textures/chao.jpg", 2);
+  carregarTextura("textures/teto.jpg", 3);
+  // Ajustar modo para a textura não escurecer
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+  float size = 5.0;
+  glPushMatrix();
+    // face frontal
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, size);
+    glEnd();    
+
+    // face traseira
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, -size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, -size);
+    glEnd();    
+
+    // face superior
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[3]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, size, size);
+    glEnd();    
+
+    // face inferior
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[2]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, -size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, -size, size);
+    glEnd();    
+
+    // face direita
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(size, -size, size);
+    glEnd();    
+
+    // face esquerda
+    if (loadHouseTex) glBindTexture(GL_TEXTURE_2D, idsTextura[1]);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0, 0.0); glVertex3f(-size, -size, -size);
+      glTexCoord2f(1.0, 0.0); glVertex3f(-size, size, -size);
+      glTexCoord2f(1.0, 1.0); glVertex3f(-size, size, size);
+      glTexCoord2f(0.0, 1.0); glVertex3f(-size, -size, size);
+    glEnd();    
+    
+  glPopMatrix();
+  
+  glDisable(GL_TEXTURE_2D);
+}
 
 void inicializa() {
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
@@ -258,70 +491,22 @@ void inicializa() {
     glShadeModel(GL_SMOOTH);
 }
 
-void desenhaMesa() {
-    GLfloat difusoMarrom[]    = {0.4f, 0.2f, 0.0f, 1.0f};
-    GLfloat ambienteMarrom[]  = {0.2f, 0.1f, 0.0f, 1.0f};
-    GLfloat especularMarrom[] = {0.0f, 0.0f, 0.0f, 1.0f}; 
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, difusoMarrom);
-    glMaterialfv(GL_FRONT, GL_AMBIENT, ambienteMarrom);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, especularMarrom);
-    glMaterialf(GL_FRONT, GL_SHININESS, 0.0f);
-
-    // Tampo da mesa
-    glPushMatrix();
-    glTranslatef(0.0f, 1.0f, 0.0f);
-    glScalef(4.0f, 0.2f, 2.0f);
-    glutSolidCube(1.0f);
-    glPopMatrix();
-
-    // Pés da mesa
-    float tamPé = 0.2f; 
-    float alturaPé = 1.0f;
-
-    for (float x : std::initializer_list<float>{-1.7f, 1.7f}) {
-        for (float z : std::initializer_list<float>{-0.8f, 0.8f}) {
-            glPushMatrix();
-            glTranslatef(x, alturaPé / 2, z);
-            glScalef(tamPé, alturaPé, tamPé);
-            glutSolidCube(1.0f);
-            glPopMatrix();
-        }
-    }
-
-    // Desenhar o cubo sobre a mesa se não estiver pegado
-    if (!objetoPegado) {
-      GLfloat difObj[] = {0.8f, 0.1f, 0.1f, 1.0f};
-      GLfloat ambObj[] = {0.2f, 0.05f, 0.05f, 1.0f};
-      glMaterialfv(GL_FRONT, GL_DIFFUSE, difObj);
-      glMaterialfv(GL_FRONT, GL_AMBIENT, ambObj);
-      glMaterialf(GL_FRONT, GL_SHININESS, 10.0f);
-
-      glPushMatrix();
-        glTranslatef(objetoX, objetoY, objetoZ); 
-        glutSolidCube(0.1f);
-      glPopMatrix();
-    }
-
-    // Desenhar a garra acima da mesa
-    glPushMatrix();
-      glTranslatef(0.0f, 1.243f, 0.0f); 
-      desenhaGarra(1.0f); 
-    glPopMatrix();
-}
-
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    float camX = cameraDistance * sinf(cameraAngleY * M_PI / 180.0f) * cosf(cameraAngleX * M_PI / 180.0f);
-    float camZ = cameraDistance * cosf(cameraAngleY * M_PI / 180.0f) * cosf(cameraAngleX * M_PI / 180.0f);
-    float camY = cameraDistance * sinf(cameraAngleX * M_PI / 180.0f);
-    gluLookAt(camX, camY, camZ, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    float camX = cameraDistance * cos(cameraAngleHorizontal);
+    float camY = cameraDistance * sin(cameraAngleVertical);
+    float camZ = cameraDistance * sin(cameraAngleHorizontal);
+
+    // camera
+    gluLookAt(camX, camY, camZ, // posicao da camera
+              0.0, 0.0, 0.0,    // ponto alvo da camera
+              0.0, 1.0, 0.0);   // rotacao da camera
 
     glPushMatrix();
-    glRotatef(mesaRotX, 1.0f, 0.0f, 0.0f);
-    glRotatef(mesaRotY, 0.0f, 1.0f, 0.0f);
-    glRotatef(mesaRotZ, 0.0f, 0.0f, 1.0f);
+
+    desenhaQuarto();
     desenhaMesa();
     glPopMatrix();
 
@@ -338,15 +523,15 @@ void reshape(int w, int h) {
 }
 
 void teclado(unsigned char tecla, int x, int y) {
-    const float incremento = 5.0f; 
+    const float incremento = 0.1f; 
 
     switch (tecla) {
-        case 'w': mesaRotX += incremento; break;
-        case 's': mesaRotX -= incremento; break;
-        case 'a': mesaRotY += incremento; break;
-        case 'd': mesaRotY -= incremento; break;
-        case 'q': cameraDistance += incremento; break;
-        case 'e': cameraDistance -= incremento; break;
+        case 'w': cameraAngleVertical  += incremento; break;   //
+        case 's': cameraAngleVertical  -= incremento; break;   //
+        case 'a': cameraAngleHorizontal  += incremento; break; //
+        case 'd': cameraAngleHorizontal  -= incremento; break; //
+        case 'q': cameraDistance += incremento; break;         //
+        case 'e': cameraDistance -= incremento; break;         //
     }
 
     if (tecla == '1')
